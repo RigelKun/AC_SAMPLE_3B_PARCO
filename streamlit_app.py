ff6b18adef0b69bc0aa144cb929a9b47ca04ee42
Rigel Parco <riparco@my.cspc.edu.ph>
2025-03-27 04:21:27 +0000

    Button_trials

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app - RIGEL PARCO")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.button("I hate you", type="primary")
+if st.button("I love you"):
+    st.write("I love you too")
+else:
+    st.write("I love you though")
+
+st.button("Dont click it", type="primary")
+st.write("[click this link](https://www.youtube.com/watch?v=dQw4w9WgXcQ).")
