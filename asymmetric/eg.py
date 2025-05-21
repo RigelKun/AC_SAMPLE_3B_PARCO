59ed8ebc87bb4c9b3859c72ca22127b4d300966c
Rigel Parco <riparco@my.cspc.edu.ph>
2025-05-21 11:57:09 +0800

    Commit to remove ECC(Eliptic Curve) and finalize asymmetric page

--- asymmetric/eg.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def render():
-    st.title("ElGamal")
-    st.write("Project in Applied Cryptography")
-    
-    if st.button("⬅️ Back to Asymmetric Menu"):
-        st.session_state.asym_page = "menu"
-        st.rerun()
